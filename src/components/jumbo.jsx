b1bb31b4b4b537e9b773f7f1e17e1f3a4414980a
DannyABoat <dannysum07@yahoo.com>
2022-01-25 18:09:40 +0000

    updated

--- src/components/jumbo.jsx
+++ src/components/jumbo.jsx
@@ -3,7 +3,7 @@ const Jumbotron = () => {
         <div id="jumbotron">
             <img
             style={{height: "300px" , width: "800px"}} 
-            src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/brewster-mcleod-architects-1486154143.jpg"></img>
+            src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/brewster-mcleod-architects-1486154143.jpg" alt=""></img>
             <h1>A House</h1>
         </div>
 
